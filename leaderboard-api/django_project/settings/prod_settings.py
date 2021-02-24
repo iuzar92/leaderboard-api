1aa18072fdbbf710fd10197f0e0bb57b1fae3a5e
Ibrahim Uzar <uzar.ibrahim@gmail.com>
2021-02-25 00:45:27 +0300

    Update prod_settings.py

--- leaderboard-api/django_project/settings/prod_settings.py
+++ leaderboard-api/django_project/settings/prod_settings.py
@@ -9,9 +9,6 @@ print("Using Production.")
 ALLOWED_HOSTS = ['*']
 DEBUG = False
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1=x5=#g_p&&su9#6yszcyl&z7#^#@64v^)rv(rdgz*o+%1!jv1'
-
 # Production Proxy Database
 DATABASES = {
     'default': {
